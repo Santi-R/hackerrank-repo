fc2c557155cba9d6ae83dbcbbf81cdc825d75a10
Santi-R <srivascobas@gmail.com>
2018-10-19 19:54:42 +0200

    Ejercicio introductorio de Hacker rank al uso de la clase Scanner

---
+++ java-proficiency/EjercicioIntroductorioScanner.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt(); 
+        scan.close();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        //Debug 
+        System.err.println(" valor de a "+ a+ " Valor de b " +b + " valor de c " + c); 
+    }
+}
+
